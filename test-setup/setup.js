92f4f6759e5a53ef43e0a10a11860898ace78e1e
Alexandre Muniz <aom@puzzlezapper.com>
2020-01-10 22:41:36 -0800

    refactored test setup

--- test-setup/setup.js
+++ test-setup/setup.js
@@ -1,190 +1,32 @@
 const mongoose = require('mongoose');
-const Dance = require('../lib/models/Dance');
-const DancePerformance = require('../lib/models/DancePerformance');
-const Dancer = require('../lib/models/Dancer');
-const Gig = require('../lib/models/Gig');
-const Team = require('../lib/models/Team');
-const User = require('../lib/models/User');
-const Competency = require('../lib/models/Competency');
+const connect = require('../lib/utils/connect');
+const seedData = require('./seed-data');
+const seedUsers = require('./seed-users');
+const models = [
+  require('../lib/models/Team'),
+  require('../lib/models/Gig'), 
+  require('../lib/models/Competency'), 
+  require('../lib/models/DancePerformance'), 
+  require('../lib/models/Dancer'),
+  require('../lib/models/Dance'),
+];
 
-const testSetup = async() => {
-  let dances;
-  let dancers;
-  let dancePerformances;
-  let gigs;
-  let squireUser;
-  let dancerUser;
-  let adminUser;
-  let competencies;
+const testObj = {};
 
+beforeAll(async() => {
+  await connect();
   await mongoose.connection.dropDatabase();
-  const testDancer = await Dancer.create({
-    name: 'Will K',
-  });
+  testObj.testUsers = await seedUsers();
+});
 
-  adminUser = await User.create({
-    email: 'admin@test.com',
-    password: 'password',
-    role: 'admin',
-    // note: no associated dancer object
-  });  
+beforeEach(async() => {
+  // Silently ignore the errors from dropping collections that don't exist. 
+  await Promise.all(models.map(model => model.collection.drop().catch(() => true)));
+  testObj.testData = await seedData(testObj.testUsers);
+});
 
-  squireUser = await User.create({
-    email: 'squire@test.com',
-    password: 'password',
-    role: 'squire',
-    dancer: testDancer._id
-  });
+afterAll(() => {
+  return mongoose.connection.close();
+});
 
-  const squireUser2 = await User.create({
-    email: 'squire2@test.com',
-    password: 'password',
-    role: 'squire',
-    dancer: testDancer._id
-  });
-
-  dancerUser = await User.create({
-    email: 'dancer@test.com',
-    password: 'password',
-    role: 'dancer',
-    dancer: testDancer._id
-  });
-
-  dances = await Dance.create([
-    {
-      name: 'Oak and Ash and Thorn',
-      tradition: 'Moulton',
-      dancerQuantity: 6
-    },
-    {
-      name: 'South Australia',
-      tradition: 'Adderbury',
-      dancerQuantity: 8
-    },
-    {
-      name: 'Simon\'s Fancy',
-      tradition: 'Bampton',
-      dancerQuantity: 4
-    },
-  ]);
-  dancers = await Dancer.create([
-    { name: 'Ali M' },
-    { name: 'Linda G' },
-    { name: 'David S' },
-    { name: 'Wilson A' },
-    { name: 'Debbi I' },
-    { name: 'Azalea M' },
-    { name: 'Joan Z' },
-    { name: 'Scott T' }
-  ]);
-  const teams = await Team.create(
-    [
-      {
-        squire: squireUser._id,
-        name: 'Bridgetown Morris Men',
-        dancers: [
-          testDancer._id,
-          dancers[0]._id,
-          dancers[1]._id,
-          dancers[2]._id,
-          dancers[3]._id,
-        ],
-      },
-      {
-        squire: squireUser2._id,
-        name: 'Renegade Rose Morris',
-        dancers: [
-          dancers[4]._id,
-          dancers[5]._id,
-          dancers[6]._id,
-          dancers[7]._id,
-        ],
-      }
-    ]
-  );
-
-  gigs = await Gig.create([
-    {
-      name: 'Paganfaire 2020',
-      date: new Date(2020, 2, 10),
-      team: teams[0]._id
-    },
-    {
-      name: 'Mayday 2020',
-      date: new Date(2020, 4, 1),
-      team: teams[0]._id
-    },
-  ]);
-  dancePerformances = await DancePerformance.create([
-    {
-      dance: dances[0].id,
-      dancers: [
-        dancers[0].id,
-        dancers[1].id,
-        dancers[2].id,
-        dancers[3].id,
-        dancers[4].id,
-        dancers[5].id,
-      ],
-      gig: gigs[0].id,
-      team: teams[0]._id
-    },
-    {
-      dance: dances[1].id,
-      dancers: [
-        dancers[0].id,
-        dancers[1].id,
-        dancers[2].id,
-        dancers[3].id,
-        dancers[4].id,
-        dancers[5].id,
-        dancers[6].id,
-        dancers[7].id,
-      ],
-      gig: gigs[0].id,
-      team: teams[0]._id
-    },
-    {
-      dance: dances[2].id,
-      dancers: [
-        dancers[0].id,
-        dancers[1].id,
-        dancers[2].id,
-        dancers[3].id,
-      ],
-      gig: gigs[0].id,
-      team: teams[0]._id
-    }
-  ]);
-
-  competencies = await Competency.create(
-    [
-      {
-        dance: dances[0]._id,
-        dancer: dancerUser.dancer,
-        levels: ['novice', 'novice',
-          'intermediate', 'intermediate',
-          'novice', 'novice']
-      },
-      {
-        dance: dances[1]._id,
-        dancer: dancers[0]._id,
-        levels: ['proficient', 'proficient',
-          'proficient', 'proficient',
-          'proficient', 'proficient',
-          'proficient', 'proficient']
-      },
-      {
-        dance: dances[2]._id,
-        dancer: dancers[5]._id,
-        levels: ['intermediate', 'proficient',
-          'proficient', 'intermediate']
-      }
-    ]
-  );
-
-  dancers = await Promise.all(dancers.map((dancer) => Dancer.findById(dancer._id)));
-  return { dances, dancers, dancePerformances, gigs, teams, squireUser, dancerUser, adminUser, competencies };
-};
-
-module.exports = { testSetup };
+module.exports = testObj;
